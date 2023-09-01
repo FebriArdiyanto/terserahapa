815f357e1c665a98883be0cb7608646d318d9728
FebriArdiyanto <yantofebri658@gmail.com>
2023-09-01 14:27:18 +0700

    penambahan method nama untuk print nama sevri

--- app/src/main/java/com/febri/coba2/MainActivity.java
+++ app/src/main/java/com/febri/coba2/MainActivity.java
@@ -11,4 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void nama(){
+        System.out.println("sevri vendrian A.j");
+    }
 }
